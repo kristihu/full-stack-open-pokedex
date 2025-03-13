e039b5a79458ffe2e9f0b4c0079d99f8f5a068c0
kristihu <kristihu@metropolia.fi>
2025-03-13 19:41:56 +0200

    build & test

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -33,14 +33,15 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find(
     (ability) => ability.is_hidden === true
   );
-  // eslint-disable-next-line no-console
+  /* eslint-disable no-console */
   console.log("hiddenAbility=", hiddenAbility);
+  /* eslint-enable no-console */
   return (
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div
