5fc0140df7cbcc0dc52693f86190d6b4bf5076df
kristihu <kristihu@metropolia.fi>
2025-03-13 20:43:29 +0200

    cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,4 +7,12 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+
+  it("should navigate to ivysaur page and display abilities", function () {
+    cy.visit("http://localhost:5000");
+
+    cy.contains("ivysaur").click();
+
+    cy.contains("chlorophyll").should("be.visible");
+  });
 });
