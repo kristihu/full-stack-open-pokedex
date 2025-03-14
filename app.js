b9b361c3043cde053b6820047d967cba27c742ab
kristihu <kristihu@metropolia.fi>
2025-03-14 11:34:00 +0200

    fly.io

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
+app.get("/version", (req, res) => {
+  res.send("1"); // Change this number to verify new deployments
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
