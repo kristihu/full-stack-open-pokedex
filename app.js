875d9a6c689c57ed1f826aa6b576b12a53f427c1
kristihu <kristihu@metropolia.fi>
2025-03-14 12:21:36 +0200

    flyio

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ app.get("/version", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw "error...  ";
   res.send("ok");
 });
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
