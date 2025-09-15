0718098dbda5ec19bc2e052ff15b7c82227db816
kristihu <kristihu@metropolia.fi>
2025-09-15 17:40:23 +0300

    eslint pass

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,9 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
+    "linebreak-style": 0,
     quotes: ["error", "double"],
-    semi: ["error", "never"],
+    semi: ["error", "always"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
