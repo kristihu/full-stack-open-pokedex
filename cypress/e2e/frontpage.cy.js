13b0a95a419dc2f6ae4d9b7bd29f82a838a29c91
kristihu <kristihu@metropolia.fi>
2025-09-15 18:11:56 +0300

    e2e tests cypress

---
+++ cypress/e2e/frontpage.cy.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+describe("Pokedex", function () {
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+});
+describe("Pokedex Navigation", function () {
+  it("can navigate from main page to a Pokémon page", function () {
+    cy.visit("http://localhost:5000");
+
+    cy.contains("ivysaur").click();
+
+    cy.contains("chlorophyll");
+  });
+});
